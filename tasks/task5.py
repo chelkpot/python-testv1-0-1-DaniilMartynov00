e3575aedd4b69381bdd9cdd0c966b85461f4bcd6
DaniilMartynov00 <busyy075@gmail.com>
2025-09-28 20:54:47 +0500

    Я ВСЕ СДЕЛАЛ

--- tasks/task5.py
+++ tasks/task5.py
@@ -4,6 +4,10 @@
 
 def solve():
 # Ниже пишите решение задачии(Обязательно поставьте четыре пробела после функции Solve())
+    numbers_input = input("Введите числа, разделённые пробелами: ")
+    numbers = map(int, numbers_input.split())
+    squares = [num ** 2 for num in numbers]
+    print("Результат:", ' '.join(map(str, squares)))
 
 
 # Код ниже не трогать! он нужен для тестов
